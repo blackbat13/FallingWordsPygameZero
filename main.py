017a7e566b2fc4c7ef62cb9cc6e428a1f089499c
blackbat13 <blackbat13@gmail.com>
2023-05-11 18:49:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pgzrun
-from random import random, randint
+from random import random, randint, choice
 from faker import Faker
 
 
@@ -10,36 +10,71 @@ fake = Faker()
 
 words_list = []
 
+game = {
+        "points": 0,
+        "level": 1,
+        "word_count": 0
+    }
+
+colors_list = ["#E90064", "#FFED00", "#16FF00", "#00F5FF", "#FF8D29", "#F9D371", "#1CC5DC"] 
+
 
 def draw():
-    screen.fill("white")
+    screen.fill("#191825")
 
     for word in words_list:
-        screen.draw.text(word["word"], center=(word["x"], word["y"]), color="black", fontsize=40)
+        screen.draw.text(word["word"], center=(word["x"], word["y"]), color=word["color"], fontsize=40)
+
+    screen.draw.text(f"Word count: {game['word_count']}", midleft=(5, HEIGHT - 80), color="#F3CCFF", fontsize=30)
+    screen.draw.text(f"Level: {game['level']}", midleft=(5, HEIGHT - 50), color="#865DFF", fontsize=30)
+    screen.draw.text(f"Points: {game['points']}", midleft=(5, HEIGHT - 20), color="#A31ACB", fontsize=30)
 
 
 def update():
-    if random() < 0.01:
-        words_list.append({
-            "word": fake.word().lower(), 
-            "x": randint(100, WIDTH - 100), 
-            "y": -50, 
-            "v": random() + 0.05})
-        
     for word in words_list[:]:
         word["y"] += word["v"]
-        if word["y"] > HEIGHT + 100:
-            words_list.remove(word)
+        if word["y"] > HEIGHT + 20:
+            game["points"] = 0
+            game["level"] = 0
+            game["word_count"] = 0
+            words_list.clear()
+            break
 
 
 def on_key_down(key):
     character = chr(key)
-    for word in words_list[:]:
-        if word["word"][0] == character:
-            word["word"] = word["word"][1:]
-            if len(word["word"]) == 0:
-                words_list.remove(word)
-            break
+    max_y = 0
+    max_word_index = -1
+    for index, word in enumerate(words_list):
+        if word["word"][0] == character and word["y"] > max_y:
+            max_word_index = index
+            max_y = word["y"]
+
+    if max_word_index == -1:
+        game["points"] -= 1
+        return
+    
+    words_list[max_word_index]["word"] = words_list[max_word_index]["word"][1:]
+    if len(words_list[max_word_index]["word"]) == 0:
+        words_list.pop(max_word_index)
+        game["points"] += 5 * game["level"]
+        game["word_count"] += 1
+        if game["word_count"] % 10 == 0:
+            game["level"] += 1
 
 
+def add_word():
+    for _ in range(game["level"]):
+        words_list.append({
+                "word": fake.word().lower(), 
+                "x": randint(50, WIDTH - 50), 
+                "y": randint(-40, -20), 
+                "v": random()/2 + 0.1 * game["level"],
+                "color": choice(colors_list)
+                })
+    
+    clock.schedule(add_word, random() * 5 + 1)
+
+
+add_word()
 pgzrun.go()
